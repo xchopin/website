f7efa411ea2c39ce464c2bd2ad9a51745707c853
Xavier Chopin <xchopin54@gmail.com>
2020-02-05 22:30:13 +0100

    feat: add age calculator

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,13 +8,18 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  @ViewChild('typewriter', {static: true}) typewriterElement;
-
   ngOnInit() {
 
   }
 
   constructor() {
+  }
+
+
+  calculateAge() {
+    const birthday = +new Date('02-26-1995');
+    // tslint:disable-next-line:no-bitwise
+    return ~~((Date.now() - birthday) / (31557600000));
   }
 
 
